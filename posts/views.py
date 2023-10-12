726a51c0eeb6ca26b9c7ed4b1698e009ed194759
Mohamed <m7mdellham77@gmail.com>
2023-10-12 18:56:47 +0300

    add another html page

--- posts/views.py
+++ posts/views.py
@@ -6,3 +6,7 @@ def post_list(request):
     data=post.objects.all() # orm --> sql--> db --> list(all posts)
     return render(request , 'posts.html' , {'posts':data})
   
+
+def post_details(request,post_id):
+    data=post.objects.get(id=post_id)
+    return render(request,'post_detail.html',{'posts':data})
