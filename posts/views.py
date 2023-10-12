e2ed0c37c33309ce5eaf922f995c89bed787123e
Mohamed <m7mdellham77@gmail.com>
2023-10-12 23:02:30 +0300

    add post_detail

--- posts/views.py
+++ posts/views.py
@@ -7,6 +7,6 @@ def post_list(request):
     return render(request , 'posts.html' , {'posts':data})
   
 
-def post_details(request,post_id):
+def post_detail(request,post_id):
     data=post.objects.get(id=post_id)
-    return render(request,'post_detail.html',{'posts':data})
+    return render(request,'post_detail.html',{'post':data})
