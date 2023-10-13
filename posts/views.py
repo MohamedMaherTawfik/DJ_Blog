2217e49bdce0f1fa542955f76cff698d70054b2a
Mohamed <m7mdellham77@gmail.com>
2023-10-13 15:31:33 +0300

    add edit post

--- posts/views.py
+++ posts/views.py
@@ -24,3 +24,18 @@ def add_post(request):
     else:    
         form=postform()
     return render(request,'new.html',{'form':form})
+
+
+
+def edit_post(request,post_id):
+    data=post.objects.get(id=post_id)
+    if request.method == 'POST':
+        form=postform(request.POST,request.FILES,instance=data)
+        if form.is_valid():
+            myform=form.save(commit=False)
+            myform.author=request.user
+            myform.save()
+            return redirect('/blog/')
+    else:    
+        form=postform(instance=data)
+    return render(request,'edit.html',{'form':form})
